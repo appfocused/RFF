367d45a54886bbc0cf748be54b9e48fdc67ea998
AppFocused <info@appfocused.com>
2012-10-04 01:25:57 +0100

    Venues screen + some graphics added

---
+++ RFF/RFFVenuesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RFFVenuesViewController.h
+//  RFF
+//
+//  Created by Vitaly Kondratiev on 04/10/2012.
+//  Copyright (c) 2012 AppFocused. All rights reserved.
+//
+
+#import "RFFScreenViewController.h"
+
+@interface RFFVenuesViewController : RFFScreenViewController
+
+@end
---
+++ RFF/RFFVenuesViewController.m
@@ -0,0 +1,38 @@
+//
+//  RFFVenuesViewController.m
+//  RFF
+//
+//  Created by Vitaly Kondratiev on 04/10/2012.
+//  Copyright (c) 2012 AppFocused. All rights reserved.
+//
+
+#import "RFFVenuesViewController.h"
+
+@interface RFFVenuesViewController ()
+
+@end
+
+@implementation RFFVenuesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
