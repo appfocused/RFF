e175a8a904bc67aa3ddf06797e4b093092e94ec5
AppFocused <info@appfocused.com>
2012-10-06 01:09:13 +0100

    List films and detailed screen stabs added

---
+++ RFF/RFFFilmDetail.h
@@ -0,0 +1,13 @@
+//
+//  RFFFilmDetail.h
+//  RFF
+//
+//  Created by Vitaly Kondratiev on 06/10/2012.
+//  Copyright (c) 2012 AppFocused. All rights reserved.
+//
+
+#import "RFFScreenViewController.h"
+
+@interface RFFFilmDetail : RFFScreenViewController
+
+@end
---
+++ RFF/RFFFilmDetail.m
@@ -0,0 +1,38 @@
+//
+//  RFFFilmDetail.m
+//  RFF
+//
+//  Created by Vitaly Kondratiev on 06/10/2012.
+//  Copyright (c) 2012 AppFocused. All rights reserved.
+//
+
+#import "RFFFilmDetail.h"
+
+@interface RFFFilmDetail ()
+
+@end
+
+@implementation RFFFilmDetail
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
